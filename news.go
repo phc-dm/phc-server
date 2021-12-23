1a27b2d91e4b4c0424e509b15ba12c0ed902c1ab
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2021-12-23 15:35:57 +0100

    Stili per i contenuti renderizzati da markdown come immagini e blocchi di codice

--- news.go
+++ news.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"path"
 	"sort"
@@ -12,21 +13,50 @@ import (
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark/util"
 
+	chromahtml "github.com/alecthomas/chroma/formatters/html"
 	mathjax "github.com/litao91/goldmark-mathjax"
+	highlighting "github.com/yuin/goldmark-highlighting"
 
 	"gopkg.in/yaml.v3"
 )
 
 var md goldmark.Markdown
 
+// https://github.com/yuin/goldmark-highlighting/blob/9216f9c5aa010c549cc9fc92bb2593ab299f90d4/highlighting_test.go#L27
+func customWrapper(w util.BufWriter, c highlighting.CodeBlockContext, entering bool) {
+	lang, ok := c.Language()
+	if entering {
+		if ok {
+			w.WriteString(fmt.Sprintf(`<pre class="%s"><code>`, lang))
+			return
+		}
+		w.WriteString("<pre><code>")
+	} else {
+		if ok {
+			w.WriteString("</pre></code>")
+			return
+		}
+		w.WriteString("</pre></code>")
+	}
+}
+
 func init() {
+
 	md = goldmark.New(
 		goldmark.WithExtensions(
 			extension.GFM,
 			extension.Typographer,
 			// Questo pacchetto ha un nome stupido perché in realtà si occupa solo del parsing lato server del Markdown mentre lato client usiamo KaTeX.
 			mathjax.NewMathJax(),
+			highlighting.NewHighlighting(
+				highlighting.WithStyle("github"),
+				highlighting.WithWrapperRenderer(customWrapper),
+				highlighting.WithFormatOptions(
+					chromahtml.PreventSurroundingPre(true),
+				),
+			),
 		),
 		goldmark.WithParserOptions(
 			parser.WithAutoHeadingID(),
